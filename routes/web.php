2b851c1efafe1f73bedccef02850967863c09e72
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-01-09 22:25:25 +0200

    get data from db

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MySecondController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyFirstController;
 
@@ -11,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/first-page', [MyFirstController::class, 'index']);
 Route::get('/second-page', [MySecondController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
