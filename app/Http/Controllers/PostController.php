fa1f2f8d5dbf32bb863eabc9f0939506dcc7a0c2
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-01-12 00:35:06 +0200

    add tags relationship: update Post model with tags relationship, adjust PostController for testing, and include Tag model usage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
-        $category = Category::find(1);
-        dump($category->posts);
+//        $posts = Post::all();
+//        $category = Category::find(1);
+//        dump($category->posts);
 
         $post = Post::find(1);
-        dd($post->category);
+
+        $tag= Tag::find(1);
+        dd($tag->posts);
 
 //        return view('post.index', compact('posts'));
     }
